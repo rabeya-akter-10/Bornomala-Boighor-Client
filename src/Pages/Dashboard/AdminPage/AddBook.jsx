7ba6e5daa66b25771314582eadf5125033e9af3c
ashikur-rahman-10 <ashikur.rahman3912@gmail.com>
2024-03-15 20:29:55 +0600

    Somme changes on add book page

--- src/Pages/Dashboard/AdminPage/AddBook.jsx
+++ src/Pages/Dashboard/AdminPage/AddBook.jsx
@@ -12,6 +12,7 @@ const AddBook = () => {
     const [loading, setLoading] = useState(true);
     const [showModal1, setShowModal1] = useState(false);
     const [showModal2, setShowModal2] = useState(false);
+    const [showModal3, setShowModal3] = useState(false);
     const [searchResults, setSearchResults] = useState([]);
     const [showDropdown, setShowDropdown] = useState(false);
 
@@ -36,7 +37,35 @@ const AddBook = () => {
             return res.data;
         },
     });
+    // get writers
+    const { data: writers = [], refetch: writersRefetch } = useQuery({
+        queryKey: ["writers"],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/writers`);
+            setLoading(false);
+            return res.data;
+        },
+    });
 
+    // Add new Writer
+    const handleAddNewWriter = (event) => {
+        event.preventDefault();
+        const newWriterValue = event.target.newWriter.value;
+        fetch("http://localhost:5000/writers", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ writerName: newWriterValue }), // Corrected the object key here
+        });
+
+        writersRefetch();
+        toast.success("Successfully Added");
+
+        setShowModal3(false); // Close modal after adding category
+    };
+
+    // Add New categories
     const handleAddNewCategory = (event) => {
         event.preventDefault();
 
@@ -56,6 +85,7 @@ const AddBook = () => {
         setShowModal1(false); // Close modal after adding category
     };
 
+    // Add new publications
     const handleAddNewPublication = (event) => {
         event.preventDefault();
 
@@ -74,6 +104,7 @@ const AddBook = () => {
         setShowModal2(false); // Close modal after adding category
     };
 
+    // Handle publication search
     const handlePublicationSearch = (event) => {
         const query = event.target.value
             ? event.target.value.toLowerCase()
@@ -84,6 +115,23 @@ const AddBook = () => {
         setSearchResults(filteredPublications);
         setShowDropdown(true);
     };
+
+    // Handle Writer Search
+    const handleWriterSearch = (event) => {
+        const query = event.target.value
+            ? event.target.value.toLowerCase()
+            : "";
+        const filteredWriter = writers.filter(
+            (writer) =>
+                writer &&
+                writer.writerName &&
+                writer.writerName.toLowerCase().includes(query)
+        );
+        setSearchResults(filteredWriter);
+        setShowDropdown(true);
+    };
+
+    // Handle Form input
     const {
         register,
         reset,
@@ -94,6 +142,13 @@ const AddBook = () => {
 
     const handleDropdownItemClick = (publication) => {
         setValue("publications", publication, { shouldValidate: true });
+
+        setShowDropdown(false);
+    };
+
+    // Set Writer Value
+    const handleWriterDropdown = (writer) => {
+        setValue("writerName", writer, { shouldValidate: true });
 
         setShowDropdown(false);
     };
@@ -223,18 +278,49 @@ const AddBook = () => {
                 </div>
 
                 <div className="flex md:flex-row md:gap-4 flex-col">
-                    <div className="form-control">
+                    <div className="form-control ">
                         <label className="label">
                             <span className="text-xs text-[#757575] font-medium ">
-                                Writer name
+                                Writer Name
                             </span>
                         </label>
-                        <input
-                            type="text"
-                            placeholder="writer name"
-                            {...register("writerName", { required: true })}
-                            className="border border-success rounded-sm p-1 focus:outline-none lg:w-[350px] w-[300px]"
-                        />
+                        <div className="relative">
+                            <input
+                                type="text"
+                                placeholder="Search Writer name"
+                                {...register("writerName")}
+                                className="border border-success rounded-sm p-1 focus:outline-none lg:w-[350px] w-[300px]"
+                                onChange={handleWriterSearch}
+                            />
+                            <button
+                                onClick={() => setShowModal3(true)}
+                                className="absolute inset-y-0 right-0 px-4 py-1  bg-blue-500 text-white "
+                            >
+                                New
+                            </button>
+                        </div>
+
+                        {showDropdown && (
+                            <span>
+                                {searchResults.length > 0 && (
+                                    <div className="absolute z-10  lg:w-[350px] w-[300px] bg-white border border-gray-200 rounded-sm shadow-md">
+                                        {searchResults.map((result) => (
+                                            <div
+                                                key={result._id}
+                                                className="p-2 hover:bg-gray-100 cursor-pointer"
+                                                onClick={() =>
+                                                    handleWriterDropdown(
+                                                        result.writerName
+                                                    )
+                                                }
+                                            >
+                                                {result.writerName}
+                                            </div>
+                                        ))}
+                                    </div>
+                                )}
+                            </span>
+                        )}
                     </div>
 
                     <div className="form-control ">
@@ -247,9 +333,7 @@ const AddBook = () => {
                             <input
                                 type="text"
                                 placeholder="Search publication name"
-                                {...register("publications", {
-                                    required: true,
-                                })}
+                                {...register("publications")}
                                 className="border border-success rounded-sm p-1 focus:outline-none lg:w-[350px] w-[300px]"
                                 onChange={handlePublicationSearch}
                             />
@@ -419,6 +503,38 @@ const AddBook = () => {
                             </button>
                             <button
                                 onClick={() => setShowModal2(false)}
+                                className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md ml-2"
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            )}
+            {showModal3 && (
+                <form
+                    onSubmit={handleAddNewWriter}
+                    className="fixed z-30 inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50"
+                >
+                    <div className="bg-white p-4 rounded-md flex gap-3 flex-col items-center justify-center">
+                        <h2 className="text-xl font-semibold mb-2">
+                            Add New Writer
+                        </h2>
+                        <input
+                            type="text"
+                            name="newWriter"
+                            className="border border-gray-300 rounded-md p-2 mb-2  w-72"
+                            placeholder="Enter Writers name"
+                        />
+                        <div>
+                            <button
+                                type="submit"
+                                className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                            >
+                                Add
+                            </button>
+                            <button
+                                onClick={() => setShowModal3(false)}
                                 className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md ml-2"
                             >
                                 Cancel
